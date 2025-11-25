20835ff2a42f8b9da4ec0706d200cef8d612fd1c
Max Geller <mxgeller@gmail.com>
2025-11-24 17:46:24 -0700

    Refactor Landing and NotFound Components, Remove Unused Files
    - Updated import paths for LandingComponent and NotFoundComponent to improve code organization.
    - Deleted the Landing and NotFound components along with their associated HTML, SCSS, and TypeScript files to clean up the codebase.
    - Adjusted app routing to reflect the removal of these components, enhancing overall project structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { DocViewerComponent } from './features/docs/ui/doc-viewer/doc-viewer';
 import { KeybindingsComponent } from './features/docs/pages/keybindings/keybindings';
-import { LandingComponent } from './features/landing/landing/landing';
-import { NotFoundComponent } from './features/not-found/not-found/not-found';
+import { LandingComponent } from './features/landing/landing';
+import { NotFoundComponent } from './features/not-found/not-found';
 import { SettingsComponent } from '@features/settings/settings';
 import { DocsShellComponent } from './shared/layouts/docs-layout/docs.layout';
 import { SettingsShellComponent } from './shared/layouts/settings-layout/settings.layout';
